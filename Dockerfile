2101f418c2f50afbdd204670ae6e369b8ff2c15d
pekare <carl@pekare.se>
2020-05-09 22:58:11 +0200

    Fix uid/gid

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,9 @@ VOLUME /config
 
 RUN apt-get update && \
     apt-get install -y supervisor openvpn qbittorrent-nox && \
-    mkdir -p /var/log/supervisor
+    mkdir -p /var/log/supervisor && \
+    groupadd -g 911 qbittorrent && \
+    useradd -u 911 -g 911 qbittorrent
 
 COPY openvpn/ /etc/openvpn
 COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,10 @@ chmod 600 /etc/openvpn/mullvad_userpass.txt
 echo "######## Making resolv-conf script executable"
 chmod +x /etc/openvpn/update-resolv-conf
 
+echo "######## Change UID/GID for qbittorent user"
+groupmod -o -g "${PGID}" qbittorrent
+usermod -o -u "${PUID}" qbittorrent
+chown -R qbittorrent:qbittorrent /config
+
 echo "######## Starting services with supervisord"
 exec /usr/bin/supervisord
